ffef6f33a71e357a4a9557798deeac3043880c7b
CodyVollrath <950doenut.buff@gmail.com>
2021-04-24 19:24:55 -0400

    Animal Game Initial Files

---
+++ src/edu/westga/cs3151/animalgame/controller/AnimalGameController.java
@@ -0,0 +1,5 @@
+package edu.westga.cs3151.animalgame.controller;
+
+public class AnimalGameController {
+
+}
---
+++ src/edu/westga/cs3151/animalgame/view/AnimalGameCodeBehind.java
@@ -0,0 +1,56 @@
+package edu.westga.cs3151.animalgame.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+public class AnimalGameCodeBehind {
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private Pane welcomePane;
+
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    private Menu menuFile;
+
+    @FXML
+    private MenuItem loadItem;
+
+    @FXML
+    private MenuItem saveItem;
+
+    @FXML
+    private Menu menuHelp;
+
+    @FXML
+    private MenuItem aboutItem;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    void aboutItem(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadItem(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveItem(ActionEvent event) {
+
+    }
+
+}
