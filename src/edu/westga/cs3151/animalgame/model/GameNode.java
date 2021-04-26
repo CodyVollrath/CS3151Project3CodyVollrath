f4ad2cb4b1815b15474c47004048ded7e44b0d38
CodyVollrath <950doenut.buff@gmail.com>
2021-04-25 22:06:22 -0400

    Added some resources too

---
+++ src/edu/westga/cs3151/animalgame/model/GameNode.java
@@ -0,0 +1,55 @@
+package edu.westga.cs3151.animalgame.model;
+
+public class GameNode {
+	private String value;
+	private GameNode parent;
+	private GameNode rightNode;
+	private GameNode leftNode;
+	
+	public GameNode(String value) {
+		this.value = value;
+		this.parent = null;
+		this.rightNode = null;
+		this.leftNode = null;
+	}
+	
+	public String getValue() {
+		return this.value;
+	}
+	
+	public void setValue(String value) {
+		if (value == null) {
+			throw new IllegalArgumentException("Value must not be null");
+		}
+		this.value = value;
+	}
+	
+	public boolean isLeaf() {
+		return this.rightNode == null && this.leftNode == null;
+	}
+	
+	public GameNode getParent() {
+		return this.parent;
+	}
+	
+	public GameNode getRight() {
+		return this.rightNode;
+	}
+	
+	public GameNode getLeft() {
+		return this.leftNode;
+	}
+	
+	public void setParent(GameNode node) {
+		this.parent = node;
+	}
+	
+	public void setRight(GameNode node) {
+		this.rightNode = node;
+	}
+	
+	public void setLeft(GameNode node) {
+		this.leftNode = node;
+	}
+	
+}
---
+++ src/edu/westga/cs3151/animalgame/resources/Resources.java
@@ -0,0 +1,9 @@
+package edu.westga.cs3151.animalgame.resources;
+
+public class Resources {
+	public static final String ROOT_QUESTION = "Does the animal live in water?";
+	public static final String FIRST_LEFT_ANIMAL = "Goldfish";
+	public static final String FIRST_RIGHT_ANIMAL = "Horse";
+	public static final String ASK_IF_ANIMAL_TEMPLATE = "Is the animal a %s?";
+	public static final String RADIO_BUTTON_NOT_SELECTED = "Please select yes or no!";
+}
